405443f46b43f99694bab21e7279c0579ed5ca0d
Eoin Campbell <campbeeo@tcd.ie>
2018-06-25 17:34:24 +0100

    Updated error messages

--- lang/en/tool_uploadblocksettings.php
+++ lang/en/tool_uploadblocksettings.php
@@ -28,6 +28,7 @@ $string['blockadded']            = '{$a->line} {$a->linenum} [{$a->oplabel}]: Bl
 $string['blockadderror']         = '{$a->line} {$a->linenum} [{$a->oplabel}]: Error: block "{$a->blocktitle}" ({$a->blockname}) not added to course "{$a->coursename}" ({$a->courseid}). {$a->skipped}.';
 $string['blockalreadyadded']     = '{$a->line} {$a->linenum} [{$a->oplabel}]: Block "{$a->blocktitle}" ({$a->blockname}) already added to course "{$a->coursename}" ({$a->courseid}). {$a->skipped}.';
 $string['blockdeleted']          = '{$a->line} {$a->linenum} [{$a->oplabel}]: Block "{$a->blocktitle}" ({$a->blockname}) deleted from course "{$a->coursename}" ({$a->courseid}).';
+$string['blockinstancenotfound']          = '{$a->line} {$a->linenum} [{$a->oplabel}]: Warning: Block "{$a->blocktitle}" ({$a->blockname}) not found in course "{$a->coursename}" ({$a->courseid}).';
 $string['blockdoesntexist']      = '{$a->line} {$a->linenum} [{$a->oplabel}]: Block "{$a->blocktitle}" ({$a->blockname}) not added to course "{$a->coursename}" ({$a->courseid}). {$a->skipped}.';
 $string['blockmoved']            = '{$a->line} {$a->linenum} [{$a->oplabel}]: Block "{$a->blocktitle}" ({$a->blockname}) in course "{$a->coursename}" ({$a->courseid}) moved.';
 $string['blocknotinstalled']     = '{$a->line} {$a->linenum} [{$a->oplabel}]: Block ({$a->blockname}) not installed. {$a->skipped}.';
@@ -41,9 +42,11 @@ $string['csvfile_help']          = 'The format of the CSV file must be as follow
 * The allowed operations are add, del, upd.
 * The allowed regions are side-pre and side-post.
 * The allowed weights are -10 to 10 (0 is neutral)';
+$string['fieldscannotbeblank']   = '{$a->line} {$a->linenum} [{$a->oplabel}]: Fields cannot be blank: \
+block ({$a->blockname}), course ($a->coursename), region ($a->region), weight ($a->weight). {$a->skipped}.';
 $string['heading']               = 'Upload course block settings from a CSV file';
-$string['invalidop']             = '{$a->line} {$a->linenum} [{$a->oplabel}]: Invalid operation "{$a->op}".';
 $string['operationnotvalid']     = '{$a->line} {$a->linenum} [{$a->oplabel}]: Operation not valid for block "{$a->blocktitle}" ({$a->blockname}). {$a->skipped}.';
+$string['operationunknown']      = '{$a->line} {$a->linenum} [{$a->oplabel}]: Unknown operation "{$a->op}".';
 $string['pluginname']            = 'Upload block settings';
 $string['pluginname_help']       = 'Upload block settings from a CSV file to set block settings for a range of courses in a single operation.';
 $string['privacy:metadata']      = 'The Upload block settings administration tool does not store personal data.';
